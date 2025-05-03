743753f4b706a17fea1b774abd8a79bf5fce658b
ayexhehe <alexhuntme123@gmail.com>
2025-05-04 00:37:58 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,26 +1,20 @@
-import { FloatingNav } from "@/components/ui/floating-navbar";
-import Hero from "../components/Hero";
-import Grid from "@/components/Grid";
 import { navItems } from "@/components/data";
 import dynamic from "next/dynamic";
 
-const Testimonials = dynamic(() => import("@/components/Proj"), {
-  ssr: false,
-});
-
-
-
+const FloatingNav = dynamic(() => import("@/components/ui/floating-navbar").then(mod => mod.FloatingNav), { ssr: false });
+const Hero = dynamic(() => import("../components/Hero").then(mod => mod.default), { ssr: false });
+const Grid = dynamic(() => import("@/components/Grid").then(mod => mod.default), { ssr: false });
+const Testimonials = dynamic(() => import("@/components/Proj").then(mod => mod.default), { ssr: false });
 
 export default function Home() {
   return (
-   <main className ="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden
-   mx-auto sm:px-10 px-5">
-    <div className = "max-w-7xl w-full">
-    {/*<FloatingNav navItems={navItems} />
-       <Hero />
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <FloatingNav navItems={navItems} />
+        <Hero />
         <Grid />
-        <Testimonials /> */}
-    </div>
-   </main>
+        <Testimonials />
+      </div>
+    </main>
   );
 }
