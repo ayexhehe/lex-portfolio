2f63ee14f2c3b0466cccfdccb0067f12ff707810
ayexhehe <alexhuntme123@gmail.com>
2025-05-04 00:27:14 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,9 +17,9 @@ export default function Home() {
    mx-auto sm:px-10 px-5">
     <div className = "max-w-7xl w-full">
     <FloatingNav navItems={navItems} />
-      /*<Hero />
-      <Grid />
-      <Testimonials />*/
+      {/* <Hero />
+        <Grid />
+        <Testimonials /> */}
     </div>
    </main>
   );
